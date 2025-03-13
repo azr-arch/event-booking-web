bf0ce15342d908be37c004b7da2a2114221d7fb1
Azar <79351619+azr-arch@users.noreply.github.com>
2025-03-13 15:54:13 +0530

    fix dynamic server err

--- app/(root)/(admin)/(platform)/events/page.tsx
+++ app/(root)/(admin)/(platform)/events/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/db-queries";
 
+export const dynamic = "force-dynamic";
+
 const EventsPage = async () => {
     const events = await getAllEvents();
 
