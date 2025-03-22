481730db64baf23c6a50b289a9847973f76b27c4
Azar <79351619+azr-arch@users.noreply.github.com>
2025-03-23 02:20:37 +0530

    fix build

--- app/(root)/(admin)/admin/_components/header.tsx
+++ app/(root)/(admin)/admin/_components/header.tsx
@@ -3,53 +3,50 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import UserNav from "./user-nav";
 
 const navItems = [
-    {
-        label: "Overview",
-        href: "/admin/dashboard",
-    },
-    {
-        label: "Events",
-        href: "/admin/events",
-    },
-    {
-        label: "Venues",
-        href: "/admin/venues",
-    },
-    {
-        label: "Settings",
-        href: "/admin/settings",
-    },
+  {
+    label: "Overview",
+    href: "/admin/dashboard",
+  },
+  {
+    label: "Events",
+    href: "/admin/events",
+  },
+  {
+    label: "Venues",
+    href: "/admin/venues",
+  },
+  {
+    label: "Settings",
+    href: "/admin/settings",
+  },
 ];
 
 export const Header = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-        <header className="h-16 px-6 flex items-center border-b border-neutral-200 dark:border-neutral-700 mb-6">
-            <nav>
-                <ul className="flex items-center gap-x-7">
-                    {navItems.map((item) => (
-                        <li
-                            key={item.href}
-                            className={cn(
-                                "font-medium text-sm hover:text-black dark:hover:text-white",
-                                pathname === item.href
-                                    ? "text-black dark:text-white"
-                                    : "text-neutral-400"
-                            )}
-                        >
-                            <Link href={item.href} className="text-inherit">
-                                {item.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-
-            <UserNav />
-        </header>
-    );
+  return (
+    <header className="h-16 px-6 flex items-center border-b border-neutral-200 dark:border-neutral-700 mb-6">
+      <nav>
+        <ul className="flex items-center gap-x-7">
+          {navItems.map((item) => (
+            <li
+              key={item.href}
+              className={cn(
+                "font-medium text-sm hover:text-black dark:hover:text-white",
+                pathname === item.href
+                  ? "text-black dark:text-white"
+                  : "text-neutral-400"
+              )}
+            >
+              <Link href={item.href} className="text-inherit">
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
--- app/(root)/(client)/app/page.tsx
+++ app/(root)/(client)/app/page.tsx
@@ -1,12 +1,15 @@
+import { auth } from "@/auth";
 import UserNav from "../../(admin)/admin/_components/user-nav";
 
-export default function AppPage() {
-    return (
-        <div>
-            <h1>🚧 Under Construction 🚧</h1>
-            <p>The client-side app is currently being developed.</p>
-            <p>Stay tuned for updates!</p>
-            <UserNav />
-        </div>
-    );
+export default async function AppPage() {
+  const session = await auth();
+
+  return (
+    <div>
+      <h1>🚧 Under Construction 🚧</h1>
+      <p>The client-side app is currently being developed.</p>
+      <p>Stay tuned for updates!</p>
+      <UserNav session={session} />
+    </div>
+  );
 }
